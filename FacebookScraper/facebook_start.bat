4c4a4e8046b7da3cb5561bd82d4dae5671c1ef0c
semrushmediante-dev <semrushmediante@gmail.com>
2026-04-22 15:11:30 +0200

    Delete FacebookScraper directory

--- FacebookScraper/facebook_start.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-cd /d D:\Descargas\instagram-analyzer\FacebookScraper
-
-set PLAYWRIGHT_BROWSERS_PATH=D:\playwright-browsers
-
-D:\WinPython\WPy64-3.13.12.0\python\python.exe app.py
-
-pause
